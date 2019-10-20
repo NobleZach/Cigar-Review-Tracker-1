266517363902d0027efc1a4cfdad567996990993
gberghoff <gageberghoff@gmail.com>
2019-10-19 22:08:52 -0400

    Added placeholder Application Icon
    This is a placeholder (red box) for the application icon.

--- src/gb/crt/java/Main.java
+++ src/gb/crt/java/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -43,6 +44,7 @@ public class Main extends Application {
 		Scene scene = new Scene(root);
 		scene.getStylesheets().add("/gb/crt/resources/css/stylesheet.css");
 		primaryStage.setTitle(APPLICATION_NAME);
+		primaryStage.getIcons().add(new Image("/gb/crt/resources/icons/ApplicationIcon.png"));
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
