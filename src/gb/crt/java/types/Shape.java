7c7125f53215b9f0a1683c3519798928d3ea1b7c
gberghoff <gageberghoff@gmail.com>
2019-10-20 16:55:38 -0400

    Shape adjustments
    Adjusted the shape enums adding an extra field

--- src/gb/crt/java/types/Shape.java
+++ src/gb/crt/java/types/Shape.java
@@ -10,41 +10,61 @@ public enum Shape {
 	/**************************
 	 * Shapes
 	 **************************/
-	CHURCHILL("Churchill"),
-	CIGARILLOS("Cigarillos"),
-	CORONA("Corona"),
-	CORONA_EXTRA("Corona Extra"),
-	DOUBLE_CORONA("Double Corona"),
-	GIANT("Giant"),
-	GIANT_CORONA("Giant Corona"),
-	GRAND_CORONA("Grand Corona"),
-	LONG_CORONA("Long Corona"),
-	LONG_PANATELA("Long Panatela"),
-	LONSDALE("Lonsdale"),
-	PANATELA("Panatela"),
-	PERFECTO("Perfecto"),
-	PETIT_CORONA("Petit Corona"),
-	PYRAMID("Pyramid"),
-	ROBUSTO("Robusto"),
-	SHORT_PANATELA("Short Panatela"),
-	SLIM_PANATELA("Slim Panatela"),
-	SMALL_PANATELA("Small Pantela"),
-	TORO("Toro"),
-	TORPEDO("Torpedo"),
+	CHISEL("Chisel","Figurado"),
+	CHURCHILL("Churchill", "Parejo"),
+	CIGARILLOS("Cigarillos", "Parejo"),
+
+	// Corona's
+	CORONA("Corona", "Parejo"),
+	CORONA_EXTRA("Corona Extra", "Parejo"),
+	CORONA_GORDA("Corona Gorda", "Parejo"),
+	CORONA_GRANDE("Corona Grande", "Parejo"),
+	DOUBLE_CORONA("Double Corona", "Parejo"),
+	GIANT_CORONA("Giant Corona", "Parejo"),
+	GRAN_CORONA("Gran Corona", "Parejo"),
+	PETIT_CORONA("Petit Corona", "Parejo"),
+	LONG_CORONA("Long Corona", "Parejo"),
+
+	GIANT("Giant", "Parejo"),
+	GORDO("Gordo", "Parejo"),
+	GORDITO("Gordito", "Parejo"),
+	LANCERO("Lancero", "Parejo"),
+	LONSDALE("Lonsdale", "Parejo"),
+	
+	// Panatela's
+	PANATELA("Panatela", "Parejo"),
+	SHORT_PANATELA("Short Panatela", "Parejo"),
+	SLIM_PANATELA("Slim Panatela", "Parejo"),
+	SMALL_PANATELA("Small Pantela", "Parejo"),
+	LONG_PANATELA("Long Panatela", "Parejo"),
+	
+	PERFECTO("Perfecto", "Figurado"),
+	PRESIDENTE("Presidente", "Parejo"),
+	PYRAMID("Pyramid", "Figurado"),
+	ROBUSTO("Robusto", "Parejo"),
+	ROTHSCHILD("Rothschild", "Parejo"),
+	TORO("Toro", "Parejo"),
+	TORPEDO("Torpedo", "Figurado"),
 	;
 
 	/**************************
 	 * Constructors
 	 **************************/
+	private final String name;
 	private final String shape;
 	
-	Shape(String shape) {
+	Shape(String name, String shape) {
+		this.name = name;
 		this.shape = shape;
 	}
 
 	/**************************
 	 * Getters
 	 **************************/
+	public String getName() {
+		return this.name;
+	}
+	
 	public String getShape() {
 		return this.shape;
 	}
