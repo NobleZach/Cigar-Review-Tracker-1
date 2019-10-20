a744e6c606a19f6d746a5a827cb8510b48591907
gberghoff <gageberghoff@gmail.com>
2019-10-19 20:16:59 -0400

    Update Cigar.java
    Added name & shape fields with getters/setters

--- src/gb/crt/java/model/Cigar.java
+++ src/gb/crt/java/model/Cigar.java
@@ -1,7 +1,9 @@
 package gb.crt.java.model;
 
+import gb.crt.java.types.Shape;
+
 /**
- * Cigar object
+ * Cigar object class.
  * 
  * @author Gage Berghoff
  */
@@ -11,11 +13,57 @@ public class Cigar {
 	 * Fields
 	 **************************/
 
+	private String name;
+	private Shape shape;
+
 	/**************************
 	 * Constructors
 	 **************************/
 
+	public Cigar(String name, Shape shape) {
+
+	}
+
 	/**************************
 	 * Methods
 	 **************************/
+
+	@Override
+	public String toString() {
+		return name;
+	}
+
+	/**************************
+	 * Getters/Setters
+	 **************************/
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		if (name.trim().isEmpty()) {
+			this.name = name;
+		}
+	}
+
+	/**
+	 * @return the shape enum of the cigar
+	 */
+	public Shape getShape() {
+		return shape;
+	}
+
+	/**
+	 * @param shape the shape enum of the cigar
+	 */
+	public void setShape(Shape shape) {
+		this.shape = shape;
+	}
 }
