db8230df16e4b20de6f5a18b3330eb132c5dd2b8
gberghoff <gageberghoff@gmail.com>
2019-10-19 20:00:20 -0400

    Added shapes
    Added enum values for the shapes

--- src/gb/crt/java/types/Shape.java
+++ src/gb/crt/java/types/Shape.java
@@ -7,4 +7,45 @@ package gb.crt.java.types;
  */
 public enum Shape {
 
+	/**************************
+	 * Shapes
+	 **************************/
+	CHURCHILL("Churchill"),
+	CIGARILLOS("Cigarillos"),
+	CORONA("Corona"),
+	CORONA_EXTRA("Corona Extra"),
+	DOUBLE_CORONA("Double Corona"),
+	GIANT("Giant"),
+	GIANT_CORONA("Giant Corona"),
+	GRAND_CORONA("Grand Corona"),
+	LONG_CORONA("Long Corona"),
+	LONG_PANATELA("Long Panatela"),
+	LONSDALE("Lonsdale"),
+	PANATELA("Panatela"),
+	PERFECTO("Perfecto"),
+	PETIT_CORONA("Petit Corona"),
+	PYRAMID("Pyramid"),
+	ROBUSTO("Robusto"),
+	SHORT_PANATELA("Short Panatela"),
+	SLIM_PANATELA("Slim Panatela"),
+	SMALL_PANATELA("Small Pantela"),
+	TORO("Toro"),
+	TORPEDO("Torpedo"),
+	;
+
+	/**************************
+	 * Constructors
+	 **************************/
+	private final String shape;
+	
+	Shape(String shape) {
+		this.shape = shape;
+	}
+
+	/**************************
+	 * Getters
+	 **************************/
+	public String getShape() {
+		return this.shape;
+	}
 }
