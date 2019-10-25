775455d8f8bb403a19ba1a505f1260f638ca59cf
gberghoff <gageberghoff@gmail.com>
2019-10-24 23:40:55 -0400

    Added session support for location/size
    Application tracks window location/size once closed and will open the application back up at those preferences.

--- src/gb/crt/java/Main.java
+++ src/gb/crt/java/Main.java
@@ -1,11 +1,14 @@
 package gb.crt.java;
 
+import java.util.prefs.Preferences;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 /**
  * Application entry point.
@@ -19,12 +22,22 @@ public class Main extends Application {
 	 **************************/
 
 	public static final String APPLICATION_NAME = "Cigar-Review-Tracker";
+	private Stage primaryStage;
+
+	// Preference constants
+	private Preferences pref = Preferences.userRoot().node(this.getClass().getName());
+	private static final String WINDOW_POSITION_X = "Window_Position_X";
+	private static final String WINDOW_POSITION_Y = "Window_Position_Y";
+	private static final String WINDOW_WIDTH = "Window_Width";
+	private static final String WINDOW_HEIGHT = "Window_Height";
+	private static final double DEFAULT_X = 10;
+	private static final double DEFAULT_Y = 10;
+	private static final double DEFAULT_WIDTH = 800;
+	private static final double DEFAULT_HEIGHT = 600;
 
 	/**************************
 	 * Methods
 	 **************************/
-
-	
 	/**
 	 * Application entry point.
 	 * 
@@ -47,5 +60,22 @@ public class Main extends Application {
 		primaryStage.getIcons().add(new Image("/gb/crt/resources/icons/ApplicationIcon.png"));
 		primaryStage.setScene(scene);
 		primaryStage.show();
+		
+		// Load previous sessions window cords and size from preferences.
+		primaryStage.setX(pref.getDouble(WINDOW_POSITION_X, DEFAULT_X));
+		primaryStage.setY(pref.getDouble(WINDOW_POSITION_Y, DEFAULT_Y));
+		primaryStage.setWidth(pref.getDouble(WINDOW_WIDTH, DEFAULT_WIDTH));
+		primaryStage.setHeight(pref.getDouble(WINDOW_HEIGHT, DEFAULT_HEIGHT));
+
+		this.primaryStage = primaryStage;
+	}
+	
+	@Override
+	public void stop() {
+		// Saving window cords and size to preferences.
+		pref.putDouble(WINDOW_POSITION_X, primaryStage.getX());
+		pref.putDouble(WINDOW_POSITION_Y, primaryStage.getY());
+		pref.putDouble(WINDOW_WIDTH, primaryStage.getWidth());
+		pref.putDouble(WINDOW_HEIGHT, primaryStage.getHeight());
 	}
 }
