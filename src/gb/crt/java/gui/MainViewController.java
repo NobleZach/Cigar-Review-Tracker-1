8fc08035be7af449ce85c2bd1d46bcca84bc844d
Zach Noble <zaemm2@gmail.com>
2019-10-24 23:56:24 -0400

    populated cigar brand autofill
    I created a txt file that populated the cigar autofill with mopst popular cigar brands and had the ocde read form it.

--- src/gb/crt/java/gui/MainViewController.java
+++ src/gb/crt/java/gui/MainViewController.java
@@ -1,7 +1,11 @@
 package gb.crt.java.gui;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 import org.controlsfx.control.textfield.TextFields;
 
@@ -11,19 +15,40 @@ import javafx.scene.control.TextField;
 
 /**
  * The controller class for the main view
- * 
+ *
  * @author Gage Berghoff
+ *@author Zach Noble
  */
 public class MainViewController implements Initializable {
 
 	@FXML
 	private TextField brandTf;
 
-	private String[] brandNames = { "Gage", "nobal", "geroge", "test", "geroge", "Gage", "nobal", "geroge", "test",
-			"geroge", "Gage", "nobal", "geroge", "test", "geroge", "Gage", "nobal", "geroge", "test", "geroge" };
+	private ArrayList<String> cigarBrands = new ArrayList<>();
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		TextFields.bindAutoCompletion(brandTf, brandNames);
+		cigarBrandPopulate();
+		TextFields.bindAutoCompletion(brandTf, cigarBrands);
+	}
+
+	public void cigarBrandPopulate()
+	{
+            File file = new File("src/gb/crt/resources/autofill/Cigar_Brand_Names.txt");
+            Scanner scanner;
+			try {
+				scanner = new Scanner(file);
+				while (scanner.hasNext())
+				{
+					String line = scanner.nextLine();
+				    cigarBrands.add(line);
+				}
+				scanner.close();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+
 	}
 }
