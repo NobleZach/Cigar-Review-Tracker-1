74a554c452ad4a0e027228907d22fecaff74d161
gberghoff <gageberghoff@gmail.com>
2019-10-24 21:52:48 -0400

    Textfield autocomplete

--- src/gb/crt/java/gui/MainViewController.java
+++ src/gb/crt/java/gui/MainViewController.java
@@ -5,16 +5,25 @@ import java.util.ResourceBundle;
 
 import org.controlsfx.control.textfield.TextFields;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 
 /**
  * The controller class for the main view
  * 
  * @author Gage Berghoff
  */
-public class MainViewController implements Initializable{
+public class MainViewController implements Initializable {
+
+	@FXML
+	private TextField brandTf;
+
+	private String[] brandNames = { "Gage", "nobal", "geroge", "test", "geroge", "Gage", "nobal", "geroge", "test",
+			"geroge", "Gage", "nobal", "geroge", "test", "geroge", "Gage", "nobal", "geroge", "test", "geroge" };
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
+		TextFields.bindAutoCompletion(brandTf, brandNames);
 	}
 }
